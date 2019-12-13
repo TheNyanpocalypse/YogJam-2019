f0505efc45a0b0d6a3ab2d632df44bfd906f5294
White Autumn <white@autumn.solutions>
2019-12-13 22:41:45 +0100

    Started working on grid

---
+++ Assets/NotVoxels/ConstructGrid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructGrid : MonoBehaviour
+{
+	
+	
+	public ConstructTile GetTileAt(Vector3 globalPos)
+	{
+		Vector3 localPos = this.transform.position - globalPos;
+		return null;
+	}
+}
